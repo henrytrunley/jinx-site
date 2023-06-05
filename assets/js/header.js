79db43754c2013f96327ef988603511dbb3e33b7
Henry Trunley <hjtrunley@gmail.com>
2023-06-05 11:11:10 +0100

    initial website skeleton

---
+++ assets/js/header.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.display === "flex") {
+      navbar.style.display = "none";
+    } else {
+      navbar.style.display = "flex";
+    }
+  }
+  
